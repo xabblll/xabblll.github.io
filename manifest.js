f56b326da9d8565877a0e05c3c9e5c184e11587b
Xabblll <xabblll@gmail.com>
2023-01-08 06:20:53 +0400

    manifest getter returned

--- manifest.js
+++ manifest.js
@@ -1,5 +1,10 @@
-const manifest = 
+const Manifest = 
     [
         {"path": "pages/TestBlogPage.html", "name": "Podracing"},
         {"path": "pages/TestBlogPage.html", "name": "Podracing Copy"},
     ];
+
+function getManifest()
+{
+    return Manifest;
+}
