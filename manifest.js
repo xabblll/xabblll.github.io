422bcb7c67616c23f848294089932ccd9ca2fece
Xabblll <xabblll@gmail.com>
2023-01-08 06:10:07 +0400

    Manifest moved to js file

---
+++ manifest.js
@@ -0,0 +1,10 @@
+const _manifest = 
+    [
+        {"path": "pages/TestBlogPage.html", "name": "Podracing"},
+        {"path": "pages/TestBlogPage.html", "name": "Podracing Copy"},
+    ];
+
+function getManifest()
+{
+    return _manifest;
+}
