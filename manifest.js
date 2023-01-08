20303fc1e21a44f96b35fb90c98fd5f0f0492ecb
Xabblll <xabblll@gmail.com>
2023-01-08 06:14:35 +0400

    Manifest clean-up

--- manifest.js
+++ manifest.js
@@ -1,10 +1,5 @@
-const _manifest = 
+const manifest = 
     [
         {"path": "pages/TestBlogPage.html", "name": "Podracing"},
         {"path": "pages/TestBlogPage.html", "name": "Podracing Copy"},
     ];
-
-function getManifest()
-{
-    return _manifest;
-}
